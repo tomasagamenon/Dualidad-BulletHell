ca910ed210812e8c7517216c542269ce5a28f1f5
tomasagamenon <tomas.agamenon@gmail.com>
2022-01-30 03:48:28 -0300

    Menu

---
+++ Dualidad-BulletHell/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    TypeScreen screen;
+    void Start()
+    {
+        Visual_UImanager.main.SetScreen(TypeScreen.MainMenu);
+        screen = TypeScreen.MainMenu;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (screen != TypeScreen.Pause)
+            {
+                Visual_UImanager.main.SetScreen(TypeScreen.Pause);
+                screen = TypeScreen.Pause;
+            } else if(screen == TypeScreen.Pause)
+            {
+                Visual_UImanager.main.SetScreen(TypeScreen.Gameplay);
+                screen = TypeScreen.Gameplay;
+            }
+        }
+    }
+
+    public void Gamplay()
+    {
+        Visual_UImanager.main.SetScreen(TypeScreen.Gameplay);
+        screen = TypeScreen.Gameplay;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
