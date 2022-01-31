7240a0d0fdc3b680749fcf0d75b3146426bfe688
Swimmablebee912 <agustin.quiroga.p@gmail.com>
2022-01-31 14:05:30 -0300

    Mouse animation/script

---
+++ Dualidad-BulletHell/Assets/Art/Visual/Script/UI/Visual_UImouse.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visual_UImouse : Visual_UIelement
+{
+    [SerializeField] typeMouse type;
+    [SerializeField] AnimatorOverrideController gameplay, menu;
+
+    public void SetMouse(typeMouse type_) {
+        type = type_;
+        SetAnimator("Change");
+    }
+    public void SetState(bool state) { SetAnimator(state, "Active"); }
+    public void Press() { SetAnimator("Press"); }
+    
+    void ChangeOverride() {
+        switch (type) {
+            case typeMouse.inGameplay:
+                anim.runtimeAnimatorController = gameplay;
+                break;
+            case typeMouse.inMenu:
+                anim.runtimeAnimatorController = gameplay;
+                break;
+        }
+        SetAnimator("Spawn");
+    }
+
+    public enum typeMouse { inGameplay, inMenu }
+}
